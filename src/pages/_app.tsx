501b37a8d3d963831b7ceafe9472e6278093afd5
Tomotaka Sakuma <ktmtmks@gmail.com>
2022-09-28 19:54:28 +0900

    change UI F/W to Chakra-UI from Tailwind.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,15 +8,28 @@ import type { AppType } from "next/app";
 import type { AppRouter } from "../server/router";
 import type { Session } from "next-auth";
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  brand: {
+    // 900: "#1a365d",
+    // 800: "#153e75",
+    // 700: "#2a69ac",
+  },
+};
+const theme = extendTheme({ colors });
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ChakraProvider theme={theme}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ChakraProvider>
   );
 };
 
